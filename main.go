20c0bdf8a81a9bd30e6d74bbca5e9edcb3ab915b
jhamPac <cloud@kevia.me>
2020-04-08 13:33:20 -0700

    derive function done

--- main.go
+++ main.go
@@ -1,9 +1,24 @@
 package main
 
+import (
+	"bytes"
+	"crypto/sha256"
+)
+
+type BlockChain struct {
+	blocks []*Block
+}
+
 type Block struct {
 	Hash     []byte
 	Data     []byte
 	PrevHash []byte
+}
+
+func (b *Block) DeriveHash() {
+	info := bytes.Join([][]byte{b.Data, b.PrevHash}, []byte{})
+	hash := sha256.Sum256(info)
+	b.Hash = hash[:]
 }
 
 func main() {
